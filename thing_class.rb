ca6d59948474c2709c64571ff103c67bb8037d41
George Groh <george.groh@gmail.com>
2014-08-30 20:23:29 -0700

    Inanimate object class and chests

---
+++ thing_class.rb
@@ -0,0 +1,50 @@
+require_relative "utility.rb"
+
+class Thing
+
+	attr_accessor :name, :location
+
+	def initialize(name, location, game)
+		@name = name
+		@location = location
+		@game = game
+	end
+
+end
+
+class Chest < Thing
+
+	@@chest_count = 0
+
+	def initialize(name, location, contents, game)
+		@@chest_count += 1
+		@name = name
+		@location = location
+		@contents = contents
+		@locked = false
+
+		@game = game
+		@symbol = "chest_#{@@chest_count}".to_sym
+		@game.objects[:chest] = self
+		@game.object_locations[@location] = :chest
+	end
+
+	def open(player)
+		if @locked || @contents.empty?
+			return false
+		end
+
+		message = "You picked up "
+
+		until @contents.empty?
+			item = @contents.pop
+			message += "#{item}, "
+			player.inventory[:items] << item
+		end
+
+		@game.objects.delete(:chest)
+		@game.object_locations[@location] = :empty
+		puts message
+	end
+
+end
